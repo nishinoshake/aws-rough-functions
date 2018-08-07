cd681562187c45a3b21a8039e9923a6a5e773c82
noplan1989 <lawson.and.7.11@gmail.com>
2018-08-08 01:19:23 +0900

    Node.jsを8.10にバージョンアップ

--- handler.js
+++ handler.js
@@ -2,18 +2,13 @@ const updatePrice = require('./lib/updatePrice')
 const updateFx = require('./lib/updateFx')
 const invalidate = require('./lib/invalidate')
 
-exports.price = (event, context, callback) => {
-  Promise.all([updatePrice(), updateFx()])
-    .then(() => {
-      invalidate()
-        .then(() => {
-          callback(null, 'success')
-        })
-        .catch(err => {
-          callback(err)
-        })
-    })
-    .catch(err => {
-      callback(err)
-    })
+exports.price = async (event, context, callback) => {
+  try {
+    await Promise.all([updatePrice(), updateFx()])
+    await invalidate()
+
+    callback(null, 'success')
+  } catch (err) {
+    callback(err)
+  }
 }
