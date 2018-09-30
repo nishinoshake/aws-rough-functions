b62371acad7771e2b2b3d9dc34f2145448a46e9d
noplan1989 <lawson.and.7.11@gmail.com>
2018-09-30 15:59:36 +0900

    ElastiCacheのインスタンスを増やす

--- functions/price/services/elasticache.js
+++ functions/price/services/elasticache.js
@@ -7,12 +7,11 @@ module.exports = {
       Filters: {
         location: 'Asia Pacific (Tokyo)',
         currentGeneration: 'Yes',
-        instanceFamily: 'Standard',
         cacheEngine: 'Redis'
       }
     },
     parse: priceList => {
-      return parseInstances(priceList, { index: 1, order: ['t2', 'm3', 'm4'] })
+      return parseInstances(priceList, { index: 1, order: ['t2', 'm3', 'm4', 'r3', 'r4'] })
     }
   }
 }
