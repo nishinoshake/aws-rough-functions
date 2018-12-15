a072d34ece9aa6f02f0719d91ef2586b9094354f
noplan1989 <lawson.and.7.11@gmail.com>
2018-12-15 14:28:56 +0900

    API GatewayのJSONの命名を統一

--- functions/batch/price/services/apigateway.js
+++ functions/batch/price/services/apigateway.js
@@ -2,14 +2,16 @@ const { parseRange, parseCache } = require('../parser')
 
 module.exports = {
   request: {
-    params: {
-      ServiceCode: 'AmazonApiGateway',
-      Filters: {
-        location: 'Asia Pacific (Tokyo)',
-        operation: 'ApiGatewayRequest'
-      }
-    },
-    parse: priceList => parseRange(priceList[0])
+    priceRange: {
+      params: {
+        ServiceCode: 'AmazonApiGateway',
+        Filters: {
+          location: 'Asia Pacific (Tokyo)',
+          operation: 'ApiGatewayRequest'
+        }
+      },
+      parse: priceList => parseRange(priceList[0])
+    }
   },
   cache: {
     params: {
