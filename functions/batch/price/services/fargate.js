a20f29b3fad9785b38ad7b4b8cffce3f36212d80
noplan1989 <lawson.and.7.11@gmail.com>
2019-01-13 13:48:22 +0900

    Revert "Fargateのペアを文字列で返す"
    This reverts commit 8d5bb4946024ef6fc635263e3469868a979d3e33.

--- functions/batch/price/services/fargate.js
+++ functions/batch/price/services/fargate.js
@@ -28,11 +28,11 @@ module.exports = {
   },
   pair: {
     values: {
-      '0.25': [0.5, ..._.range(1, 3)].map(v => v.toString()),
-      '0.5': _.range(1, 5).map(v => v.toString()),
-      '1': _.range(2, 9).map(v => v.toString()),
-      '2': _.range(4, 17).map(v => v.toString()),
-      '4': _.range(8, 31).map(v => v.toString())
+      '0.25': [0.5, ..._.range(1, 3)],
+      '0.5': _.range(1, 5),
+      '1': _.range(2, 9),
+      '2': _.range(4, 17),
+      '4': _.range(8, 31)
     }
   }
 }
