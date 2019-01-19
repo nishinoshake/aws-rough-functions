162c5e5ad911e6619dcdcdfa180caf86fe85ba79
noplan1989 <lawson.and.7.11@gmail.com>
2019-01-20 03:42:49 +0900

    価格取得で環境に依存している処理をハンドラーへ移す

--- functions/batch/price.js
+++ functions/batch/price.js
@@ -1,13 +1,21 @@
-const updatePrice = require('../../lib/updatePrice')
-const { sendWarning } = require('../../lib/notification/slack')
+const fs = require('fs')
+const AWS = require('aws-sdk')
+const pricing = new AWS.Pricing({ region: 'us-east-1' })
+const services = require('../../services')
+const fetchPrices = require('../../lib/fetchPrices')
+const { uploadJson } = require('../../lib/aws/s3')
 const { deploy } = require('../../lib/ci/circleci')
-const { IS_LOCAL } = process.env
+const { sendWarning } = require('../../lib/notification/slack')
+const { BUCKET_NAME, IS_LOCAL } = process.env
 
 exports.main = async (event, context, callback) => {
   try {
-    await updatePrice()
+    const prices = await fetchPrices(pricing, services)
 
-    if (!IS_LOCAL) {
+    if (IS_LOCAL) {
+      fs.writeFileSync(`${__dirname}/../../json/price.json`, JSON.stringify(prices))
+    } else {
+      await uploadJson(BUCKET_NAME, 'json/price.json', prices)
       await deploy('master')
     }
 
