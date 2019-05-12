20dde7b9a75623ca06ede1ff6c24a6a7d22f2310
noplan1989 <lawson.and.7.11@gmail.com>
2019-05-12 22:48:48 +0900

    DynamoDBのオンデマンドキャパシティを追加

--- src/services/dynamodb.ts
+++ src/services/dynamodb.ts
@@ -1,4 +1,4 @@
-import { parseRange } from '@/lib/parser'
+import { parseRange, parseFirstPrice } from '@/lib/parser'
 
 export default {
   storage: {
@@ -13,28 +13,56 @@ export default {
       parse: priceList => parseRange(priceList[0])
     }
   },
-  wcu: {
-    priceRange: {
-      params: {
-        ServiceCode: 'AmazonDynamoDB',
-        Filters: {
-          location: 'Asia Pacific (Tokyo)',
-          usagetype: 'APN1-WriteCapacityUnit-Hrs'
-        }
-      },
-      parse: priceList => parseRange(priceList[0])
+  provisioning: {
+    wcu: {
+      priceRange: {
+        params: {
+          ServiceCode: 'AmazonDynamoDB',
+          Filters: {
+            location: 'Asia Pacific (Tokyo)',
+            usagetype: 'APN1-WriteCapacityUnit-Hrs'
+          }
+        },
+        parse: priceList => parseRange(priceList[0])
+      }
+    },
+    rcu: {
+      priceRange: {
+        params: {
+          ServiceCode: 'AmazonDynamoDB',
+          Filters: {
+            location: 'Asia Pacific (Tokyo)',
+            usagetype: 'APN1-ReadCapacityUnit-Hrs'
+          }
+        },
+        parse: priceList => parseRange(priceList[0])
+      }
     }
   },
-  rcu: {
-    priceRange: {
-      params: {
-        ServiceCode: 'AmazonDynamoDB',
-        Filters: {
-          location: 'Asia Pacific (Tokyo)',
-          usagetype: 'APN1-ReadCapacityUnit-Hrs'
-        }
-      },
-      parse: priceList => parseRange(priceList[0])
+  ondemand: {
+    write: {
+      price: {
+        params: {
+          ServiceCode: 'AmazonDynamoDB',
+          Filters: {
+            location: 'Asia Pacific (Tokyo)',
+            usagetype: 'APN1-WriteRequestUnits'
+          }
+        },
+        parse: priceList => parseFirstPrice(priceList[0])
+      }
+    },
+    read: {
+      price: {
+        params: {
+          ServiceCode: 'AmazonDynamoDB',
+          Filters: {
+            location: 'Asia Pacific (Tokyo)',
+            usagetype: 'APN1-ReadRequestUnits'
+          }
+        },
+        parse: priceList => parseFirstPrice(priceList[0])
+      }
     }
   }
 }
