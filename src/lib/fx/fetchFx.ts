ebc2310a273d51bf101926c2f73fc5f082921255
Tatsuya Kinno <lawson.and.7.11@gmail.com>
2020-02-24 23:14:08 +0900

    為替の小数点を3桁に変更

--- src/lib/fx/fetchFx.ts
+++ src/lib/fx/fetchFx.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export default async function(url: string): Promise<number> {
   const res = await axios.get(url)
-  const usdjpy = Math.round(parseFloat(res.data.rates.JPY) * 100) / 100
+  const usdjpy = Math.round(parseFloat(res.data.rates.JPY) * 1000) / 1000
 
   // 明らかにおかしい為替じゃないかだけ確認
   if (!usdjpy || usdjpy < 50 || usdjpy > 150) {
