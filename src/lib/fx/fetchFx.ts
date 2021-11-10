b7684a60f176ed7d0d57a32d838030c7254f53cf
Tatsuya Kinno <kinnou@mefilas.com>
2021-11-10 20:54:15 +0900

    昨日の為替を取得
    最新のエンドポイントがキャッシュされていたので、明示的に日付を指定して取得する

--- src/lib/fx/fetchFx.ts
+++ src/lib/fx/fetchFx.ts
@@ -1,8 +1,17 @@
 import axios from 'axios'
+import dayjs from 'dayjs'
+import 'dayjs/locale/ja'
 
-export default async function(url: string): Promise<number> {
+dayjs.locale('ja')
+
+export default async function(endpoint: string): Promise<number> {
+  const yesterday = dayjs().add(-1, 'day').format('YYYY-MM-DD')
+  const url = `${endpoint}/${yesterday}?base=USD`
   const res = await axios.get(url)
   const usdjpy = Math.round(parseFloat(res.data.rates.JPY) * 1000) / 1000
+
+  console.log(url)
+  console.log(usdjpy)
 
   // 明らかにおかしい為替じゃないかだけ確認
   if (!usdjpy || usdjpy < 50 || usdjpy > 150) {
