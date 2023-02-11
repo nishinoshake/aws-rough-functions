7ae266c90067d6e92941ca46decc82a523554193
Tatsuya Kinno <kinnou@mefilas.com>
2023-02-12 02:26:19 +0900

    CircleCI調整

--- src/lib/ci/circleci.ts
+++ src/lib/ci/circleci.ts
@@ -3,8 +3,22 @@ import axios from 'axios'
 export async function deploy(branch: string): Promise<any> {
   const { CIRCLE_API_TOKEN, CIRCLE_BUILD_ENDPOINT } = process.env
 
-  await axios.post(
-    `${CIRCLE_BUILD_ENDPOINT}?circle-token=${CIRCLE_API_TOKEN}`,
-    { branch }
-  )
+  try {
+    await axios.post(
+      CIRCLE_BUILD_ENDPOINT,
+      { branch },
+      {
+        headers: {
+          'Circle-Token': CIRCLE_API_TOKEN,
+          'content-type': 'application/json'
+        }
+      }
+    )
+  } catch (e) {
+    console.error('CircleCIでエラーが発生しました')
+    console.error(`CIRCLE_BUILD_ENDPOINT: ${CIRCLE_BUILD_ENDPOINT}`)
+    console.error(e)
+
+    throw new Error(e)
+  }
 }
