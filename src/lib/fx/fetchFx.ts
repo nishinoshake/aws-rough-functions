3b8c725fa8bfba78913096084dc03bf09d8f93c0
Tatsuya Kinno <kinnou@mefilas.com>
2023-10-04 11:24:17 +0900

    為替取得調整

--- src/lib/fx/fetchFx.ts
+++ src/lib/fx/fetchFx.ts
@@ -1,17 +1,11 @@
 import axios from 'axios'
-import dayjs from 'dayjs'
-import 'dayjs/locale/ja'
 
-dayjs.locale('ja')
+const { FX_API_KEY } = process.env
 
 export default async function(endpoint: string): Promise<number> {
-  const yesterday = dayjs().add(-1, 'day').format('YYYY-MM-DD')
-  const url = `${endpoint}/${yesterday}?base=USD`
-  const res = await axios.get(url)
-  const usdjpy = Math.round(parseFloat(res.data.rates.JPY) * 1000) / 1000
+  const res = await axios.get(endpoint, { headers: { apikey: FX_API_KEY }})
 
-  console.log(url)
-  console.log(usdjpy)
+  const usdjpy = Math.round(parseFloat(res.data.quotes.USDJPY) * 1000) / 1000
 
   // 明らかにおかしい為替じゃないかだけ確認
   if (!usdjpy || usdjpy < 50 || usdjpy > 150) {
